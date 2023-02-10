f54b4bfe0a5f4e19679d6e7d51096f5fd4cb18f9
orneasme <123243041+orneasme@users.noreply.github.com>
2023-02-10 17:55:49 +0100

    added "update every second" interval

--- src/app.js
+++ src/app.js
@@ -1,32 +1,36 @@
-//Buenos Aires//
-let BuenosAiresElement = document.querySelector("#Buenos Aires");
-let BuenosAiresDate = document.querySelector(".date");
-let BuenosAiresTime = moment();
-BuenosAiresDate.innerHTML = BuenosAiresTime.format("MMMM Do YYYY");
+function SetCityTimes() {
+  //Buenos Aires//
+  let BuenosAiresElement = document.querySelector("#Buenos Aires");
+  let BuenosAiresDate = document.querySelector(".date");
+  let BuenosAiresTime = moment();
+  BuenosAiresDate.innerHTML = BuenosAiresTime.format("MMMM Do YYYY");
 
-let BATimeElement = document.querySelector("#Timezone1");
-BATimeElement.innerHTML = moment()
-  .tz("America/Buenos_Aires")
-  .format("hh:mm:ss [<small>]A [</small>]");
+  let BATimeElement = document.querySelector("#Timezone1");
+  BATimeElement.innerHTML = moment()
+    .tz("America/Buenos_Aires")
+    .format("hh:mm:ss [<small>]A [</small>]");
 
-//Zurich//
-let ZurichElement = document.querySelector("#Zurich");
-let ZurichDate = document.querySelector(".dateZurich");
-let ZurichTime = moment();
-ZurichDate.innerHTML = ZurichTime.format("MMMM Do YYYY");
+  //Zurich//
+  let ZurichElement = document.querySelector("#Zurich");
+  let ZurichDate = document.querySelector(".dateZurich");
+  let ZurichTime = moment();
+  ZurichDate.innerHTML = ZurichTime.format("MMMM Do YYYY");
 
-let ZurichTimeElement = document.querySelector("#Timezone2");
-ZurichTimeElement.innerHTML = moment()
-  .tz("Europe/Zurich")
-  .format("hh:mm:ss [<small>]A [</small>] ");
+  let ZurichTimeElement = document.querySelector("#Timezone2");
+  ZurichTimeElement.innerHTML = moment()
+    .tz("Europe/Zurich")
+    .format("hh:mm:ss [<small>]A [</small>] ");
 
-//Sydney//
+  //Sydney//
 
-let SydneyElement = document.querySelector("#Sydney");
-let SydneyDate = document.querySelector(".dateSydney");
-let SydneyTimeElement = document.querySelector("#Timezone3");
-let SydneyTime = moment().tz("Australia/Sydney");
-SydneyDate.innerHTML = SydneyTime.format("MMMM Do YYYY");
-SydneyTimeElement.innerHTML = moment()
-  .tz("Australia/Sydney")
-  .format("hh:mm:ss [<small>]A [</small>] ");
+  let SydneyElement = document.querySelector("#Sydney");
+  let SydneyDate = document.querySelector(".dateSydney");
+  let SydneyTimeElement = document.querySelector("#Timezone3");
+  let SydneyTime = moment().tz("Australia/Sydney");
+  SydneyDate.innerHTML = SydneyTime.format("MMMM Do YYYY");
+  SydneyTimeElement.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("hh:mm:ss [<small>]A [</small>] ");
+}
+SetCityTimes();
+setInterval(SetCityTimes, 1000);
